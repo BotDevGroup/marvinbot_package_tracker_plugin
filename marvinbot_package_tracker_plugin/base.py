c8c8d260b9990b763f9f343a187bf4009624cf64
Albert Espinosa <KJRamses@Gmail.com>
2017-01-05 20:29:39 -0400

    Added PicknSend
    -Added PinknSend
    -Added a command description
    -Added different invalid tracking ID error message per courier

--- marvinbot_package_tracker_plugin/base.py
+++ marvinbot_package_tracker_plugin/base.py
@@ -27,8 +27,10 @@ class PackageTrackerPlugin(Plugin):
             'response_format_noloc': '{date} {time}: {status}',
             'bmcargo_baseurl': 'http://erp-online.bmcargo.com/zz/estatus.aspx',
             'bmcargo_pattern': r'^WR01-\d{9}$',
-            'aeropaq_baseurl': 'http://erp-online.aeropaq.com/zz/Estatus.aspx',
+            'aeropaq_baseurl': 'http://erp-online.aeropaq.com/zz/estatus.aspx',
             'aeropaq_pattern': r'^WR02-\d{9}$',
+            'picknsend_baseurl': 'https://online.picknsend.com/zz/estatus.aspx',
+            'picknsend_baseurl': r'^WR13-\d{9}$',
         }
 
     def configure(self, config):
@@ -44,12 +46,18 @@ class PackageTrackerPlugin(Plugin):
                 'handler': self.handle_aeropaq,
                 'pattern': re.compile(config.get('aeropaq_pattern'),
                                       flags=re.IGNORECASE)
+            },
+            {
+                'name': 'picknsend',
+                'handler': self.handle_picknsend,
+                'pattern': re.compile(config.get('picknsend_pattern'),
+                                      flags=re.IGNORECASE)
             }
         ]
         self.config = config
 
     def setup_handlers(self, adapter):
-        self.add_handler(CommandHandler('track', self.on_track_command, command_description='Allows the user to add or remove replies.')
+        self.add_handler(CommandHandler('track', self.on_track_command, command_description='Allows the user to track couriers packages.')
                          .add_argument('id', help='Tracking ID (e.g. WR01-001231234)'))
 
     def setup_schedules(self, adapter):
@@ -85,7 +93,7 @@ class PackageTrackerPlugin(Plugin):
             responses.append("Parse error: {}".format(err))
 
         if len(responses) == 0:
-            update.message.reply_text("❌ Invalid tracking ID or no updates available at this time")
+            update.message.reply_text("❌ Invalid tracking ID for BMCargo or no updates available at this time")
         else:
             update.message.reply_text("\n".join(responses))
 
@@ -119,7 +127,41 @@ class PackageTrackerPlugin(Plugin):
             responses.append("Parse error: {}".format(err))
 
         if len(responses) == 0:
-            update.message.reply_text("❌ Invalid tracking ID or no updates available at this time")
+            update.message.reply_text("❌ Invalid tracking ID for Aeropaq or no updates available at this time")
+        else:
+            update.message.reply_text("\n".join(responses))
+            
+    def handle_picknsend(self, update, *args, **kwargs):
+        base_url = self.config.get('picknsend_baseurl')
+        _id = kwargs.get('id')
+        params = {'id': _id}
+        r = requests.get(base_url, params=params)
+        if r.status_code != 200:
+            update.message.reply_text('❌ Unable to make the request')
+            return
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        responses = []
+        try:
+            for td in soup.select("td[class=dxgv]"):
+                first_div, second_div = td.select("div")
+                status = first_div.select_one("span").contents[0]
+                date = second_div.contents[0].strip()
+                time = second_div.contents[2].strip()
+
+                if len(second_div.contents) >= 4:
+                    response_format = self.config.get('response_format')
+                    loc = second_div.contents[4].strip() if len(second_div.contents) >= 4 else 'Desconocido'
+                    response = response_format.format(status=status, date=date, time=time, loc=loc)
+                else:
+                    response_format = self.config.get('response_format_noloc')
+                    response = response_format.format(status=status, date=date, time=time)
+                responses.append(response)
+        except Exception as err:
+            responses.append("Parse error: {}".format(err))
+
+        if len(responses) == 0:
+            update.message.reply_text("❌ Invalid tracking ID for PicknSend or no updates available at this time")
         else:
             update.message.reply_text("\n".join(responses))
 
