4e2193cc49a2b385dbfb899219336b0361cf2e69
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2017-01-16 03:11:29 -0400

    Track command subscribes user to updates

--- marvinbot_package_tracker_plugin/base.py
+++ marvinbot_package_tracker_plugin/base.py
@@ -4,6 +4,7 @@ from marvinbot.handlers import CommandHandler
 from marvinbot.signals import plugin_reload
 from marvinbot.plugins import Plugin
 from marvinbot.models import User
+from marvinbot_package_tracker_plugin.models import TrackedPackage
 from bs4 import BeautifulSoup
 import logging
 import re
@@ -23,6 +24,9 @@ class PackageTrackerPlugin(Plugin):
         return {
             'short_name': self.name,
             'enabled': True,
+            'process_interval': {"minutes": 15},
+            'max_num_errors': 10,   # Max consecutive errors before deletion
+            'max_days_stalled': 7,  # Max days without updates before deletion
             'response_format': '{date} {time}: {status} @ {loc}',
             'response_format_noloc': '{date} {time}: {status}',
             'bmcargo_baseurl': 'http://erp-online.bmcargo.com/zz/estatus.aspx',
@@ -36,19 +40,19 @@ class PackageTrackerPlugin(Plugin):
     def configure(self, config):
         self.couriers = [
             {
-                'name': 'bmcargo',
+                'name': 'BMCargo',
                 'handler': self.handle_bmcargo,
                 'pattern': re.compile(config.get('bmcargo_pattern'),
                                       flags=re.IGNORECASE)
             },
             {
-                'name': 'aeropaq',
+                'name': 'Aeropaq',
                 'handler': self.handle_aeropaq,
                 'pattern': re.compile(config.get('aeropaq_pattern'),
                                       flags=re.IGNORECASE)
             },
             {
-                'name': 'picknsend',
+                'name': 'PickNSend',
                 'handler': self.handle_picknsend,
                 'pattern': re.compile(config.get('picknsend_pattern'),
                                       flags=re.IGNORECASE)
@@ -58,19 +62,21 @@ class PackageTrackerPlugin(Plugin):
 
     def setup_handlers(self, adapter):
         self.add_handler(CommandHandler('track', self.on_track_command, command_description='Allows the user to track couriers packages.')
-                         .add_argument('id', help='Tracking ID (e.g. WR01-001231234).'))
+                         .add_argument('id', help='Tracking number (e.g. WR01-001231234).'))
 
     def setup_schedules(self, adapter):
-        pass
+        process_tracked_packages.plugin = self
+        interval = self.config.get('process_interval')
+        job = self.adapter.add_job(process_tracked_packages, 'interval', **interval,
+                                   id='process_tracked_packages', name='Fetches tracked packages for processing',
+                                   replace_existing=True)
 
-    def handle_bmcargo(self, update, *args, **kwargs):
+    def handle_bmcargo(self, tracking_number):
         base_url = self.config.get('bmcargo_baseurl')
-        _id = kwargs.get('id')
-        params = {'id': _id}
+        params = {'id': tracking_number}
         r = requests.get(base_url, params=params)
         if r.status_code != 200:
-            update.message.reply_text('❌ Unable to make the request.')
-            return
+            return [None, r.status_code]
 
         soup = BeautifulSoup(r.text, 'html.parser')
         responses = []
@@ -78,8 +84,8 @@ class PackageTrackerPlugin(Plugin):
             for td in soup.select("td[class=dxgv]"):
                 first_div, second_div = td.select("div")
                 status = first_div.select_one("span").contents[0]
-                date = second_div.contents[0].strip()
-                time = second_div.contents[2].strip()
+                date = second_div.contents[0].strip().replace('.', '-')
+                time = second_div.contents[2].strip().upper()
 
                 if len(second_div.contents) >= 4:
                     response_format = self.config.get('response_format')
@@ -90,22 +96,16 @@ class PackageTrackerPlugin(Plugin):
                     response = response_format.format(status=status, date=date, time=time)
                 responses.append(response)
         except Exception as err:
-            responses.append("Parse error: {}".format(err))
+            log.error("Parse error: {}".format(err))
 
-        if len(responses) == 0:
-            update.message.reply_text("❌ Invalid tracking ID for *BMCargo* or no updates available at this time.",
-                                     parse_mode="Markdown")
-        else:
-            update.message.reply_text("\n".join(responses))
+        return ["\n".join(responses), r.status_code]
 
-    def handle_aeropaq(self, update, *args, **kwargs):
+    def handle_aeropaq(self, tracking_number):
         base_url = self.config.get('aeropaq_baseurl')
-        _id = kwargs.get('id')
-        params = {'id': _id}
+        params = {'id': tracking_number}
         r = requests.get(base_url, params=params)
         if r.status_code != 200:
-            update.message.reply_text('❌ Unable to make the request.')
-            return
+            return [None, r.status_code]
 
         soup = BeautifulSoup(r.text, 'html.parser')
         responses = []
@@ -113,8 +113,8 @@ class PackageTrackerPlugin(Plugin):
             for td in soup.select("td[class=dxgv]"):
                 first_div, second_div = td.select("div")
                 status = first_div.select_one("span").contents[0]
-                date = second_div.contents[0].strip()
-                time = second_div.contents[2].strip()
+                date = second_div.contents[0].strip().replace('.', '-')
+                time = second_div.contents[2].strip().upper()
 
                 if len(second_div.contents) >= 4:
                     response_format = self.config.get('response_format')
@@ -125,23 +125,16 @@ class PackageTrackerPlugin(Plugin):
                     response = response_format.format(status=status, date=date, time=time)
                 responses.append(response)
         except Exception as err:
-            responses.append("Parse error: {}".format(err))
+            log.error("Parse error: {}".format(err))
 
-        if len(responses) == 0:
-            update.message.reply_text("❌ Invalid tracking ID for *Aeropaq* or no updates available at this time.",
-                                     parse_mode="Markdown")
-        else:
-            update.message.reply_text("\n".join(responses))
+        return ["\n".join(responses), r.status_code]
 
-
-    def handle_picknsend(self, update, *args, **kwargs):
+    def handle_picknsend(self, tracking_number):
         base_url = self.config.get('picknsend_baseurl')
-        _id = kwargs.get('id')
-        params = {'id': _id}
+        params = {'id': tracking_number}
         r = requests.get(base_url, params=params)
         if r.status_code != 200:
-            update.message.reply_text('❌ Unable to make the request.')
-            return
+            return [None, r.status_code]
 
         soup = BeautifulSoup(r.text, 'html.parser')
         responses = []
@@ -152,29 +145,172 @@ class PackageTrackerPlugin(Plugin):
                 status = first_label.contents[0]
                 st, loc, datetime = [x.strip() for x in second_label.contents[0].split(',')]
                 date, time = [x.strip() for x in datetime.split('|')]
+                date = date.replace('.', '-')
+                time = time.upper()
                 loc = loc.upper()
                 response_format = self.config.get('response_format')
                 response = response_format.format(status=status, date=date, time=time, loc=loc)
                 responses.append(response)
         except Exception as err:
-            responses.append("Parse error: {}".format(err))
+            log.error("Parse error: {}".format(err))
 
-        if len(responses) == 0:
-            update.message.reply_text("❌ Invalid tracking ID for *PickNSend* or no updates available at this time.",
-                                     parse_mode="Markdown")
+        return ["\n".join(responses), r.status_code]
+
+    @classmethod
+    def add_tracked_package(cls, *args, **kwargs):
+        try:
+            tp = TrackedPackage(**kwargs)
+            tp.save()
+            return True
+        except Exception as ex:
+            log.error('Unable to add tracked package. Reason: {}'.format(ex.message))
+            return False
+
+    @classmethod
+    def fetch_tracked_package(cls, tracking_number):
+        try:
+            return TrackedPackage.by_tracking_number(tracking_number)
+        except:
+            return None
+
+    def subscribe(self, tracking_number, user_id, notify=False):
+        def do_notify():
+            self.adapter.bot.sendMessage(chat_id=user_id,
+                                         text="✅ You are now subscribed to receive updates for {}.".format(tracking_number))
+
+        tp = TrackedPackage.by_tracking_number(tracking_number)
+        if tp is None:
+            subscribers = [user_id]
+            tp = TrackedPackage(tracking_number=tracking_number,
+                                subscribers=subscribers)
+            tp.save()
+            if notify:
+                do_notify()
+        elif tp.date_deleted is None:
+            if user_id not in tp.subscribers:
+                tp.subscribers.append(user_id)
+                tp.save()
+                if notify:
+                    do_notify()
+            else:
+                if notify:
+                    self.adapter.bot.sendMessage(chat_id=user_id,
+                                                 text="❌ You are already subscribed.")
         else:
-            update.message.reply_text("\n".join(responses))
-
+            if notify:
+                self.adapter.bot.sendMessage(chat_id=user_id,
+                                             text="❌ You can no longer subscribe to this package.")
 
     def on_track_command(self, update, *args, **kwargs):
         _id = kwargs.get('id')
-
+        msg = update.message.reply_text("⌛ Parsing tracking number {}...".format(_id))
         handled = False
+        message = {
+            "message_id": msg.id,
+            "chat_id": self.update.message.chat.id,
+            "parse_mode": "Markdown"
+        }
+        if not any([courier['pattern'].match(_id) for courier in self.couriers]):
+            message["text"] = "❌ Given tracking number is not supported."
+            self.adapter.bot.editMessageText(**message)
+
         for courier in self.couriers:
             m = courier['pattern'].match(_id)
-            if m:
-                courier['handler'](update, *args, **kwargs)
-                handled = True
-                break
-        if not handled:
-            update.message.reply_text("❌ Given tracking ID is not supported.")
+            if not m:
+                continue
+
+            message["text"] = "⌛ Fetching updates from {} for {}...".format(courier['name'], _id)
+            self.adapter.bot.editMessageText(**message)
+
+            result, status = courier['handler'](tracking_number)
+            if status == 200:
+                if len(result) == 0:
+                    message["text"] = "❌ Invalid tracking number for *{}* or no updates available at this time.".format(courier['name'])
+                    self.adapter.bot.editMessageText(**message)
+                else:
+                    message["text"] = result
+                    self.adapter.bot.editMessageText(**message)
+                    user_id = update.message.from_user.id
+                    self.subscribe(_id, user_id, True)
+            else:
+                message["text"] = "❌ Service is unavailable for {} at this time. Please try later.".format(courier['name'])
+                self.adapter.bot.editMessageText(**message)
+
+            break
+
+
+def process_tracked_packages():
+    plugin = process_tracked_packages.plugin
+
+    def notify_subscribers(trackedPackage, message):
+        for subscriber in trackedPackage.subscribers:
+            plugin.adapter.bot.sendMessage(chat_id=subscriber,
+                                           text=message,
+                                           parse_mode='Markdown')
+
+    def process_tracked_package(trackedPackage):
+        log.info("Processing {}".format(trackedPackage.tracking_number))
+        tracking_number = trackedPackage.tracking_number
+        # If no subscribers, skip
+        if len(trackedPackage.subscribers) == 0:
+            log.info('No subscribers for {}. Skipping'.format(tracking_number))
+            return
+        # Delete TrackedPackage and return if tracking number isn't valid
+        if not any([courier['pattern'].match(tracking_number) for courier in plugin.couriers]):
+            trackedPackage.date_deleted = localized_date()
+            trackedPackage.save()
+            log.error('Deleted TrackedPackage {} because tracking number {} did not match any couriers.'.format(trackedPackage._id, tracking_number))
+            return
+
+        for courier in plugin.couriers:
+            m = courier['pattern'].match(tracking_number)
+            if not m:
+                continue
+
+            # Call handler
+            result, status = courier['handler'](tracking_number)
+            # Update last time the page was fetched
+            trackedPackage.date_page_fetched = localized_date()
+            trackedPackage.save()
+            if status == 200:
+
+                if len(result) == 0:
+                    # Delete TrackedPackage if no results and notify subscribers
+                    trackedPackage.date_deleted = localized_date()
+                    trackedPackage.save()
+                    notify_subscribers(trackedPackage, "🚮 You have been unsubscribed from updates for {}.\nNo updates are available.".format(tracking_number))
+
+                elif result != trackedPackage.updates:
+                    # Notify subscribers of new results
+                    trackedPackage.updates = result
+                    trackedPackage.num_errors = 0
+                    trackedPackage.date_updated = localized_date()
+                    trackedPackage.save()
+                    notify_subscribers(trackedPackage, "*Updates for {}:*\n\n{}".format(tracking_number, result))
+                else:
+                    dtu = localized_date() - trackedPackage.date_updated if trackedPackage.date_updated is not None else None
+                    if dtu is not None and (dtu.total_seconds() / (24 * 60 * 60)) >= plugin.config.get('max_days_stalled'):
+                        trackedPackage.date_deleted = localized_date()
+                        trackedPackage.save()
+                        notify_subscribers(trackedPackage, "🚮 You have been unsubscribed from updates for {}.\nNo updates since {}.".format(tracking_number, trackedPackage.date_updated))
+                    # No change, verify dates
+            else:
+                # Delete TrackedPackage if too many errors and notify subscribers
+                if trackedPackage.num_errors is not None and trackedPackage.num_errors > plugin.config.get('max_num_errors'):
+                    trackedPackage.date_deleted = localized_date()
+                    trackedPackage.save()
+                    notify_subscribers(trackedPackage, "🚮 You have been unsubscribed to receive updates for {}.".format(tracking_number))
+                else:
+                    # Increase num errors
+                    trackedPackage.num_errors = trackedPackage.num_errors + 1 if trackedPackage.num_errors is not None else 1
+                    trackedPackage.save()
+
+            break
+
+    log.info("Processing tracked packages")
+    tps = TrackedPackage.all()
+    log.info("Found {} tracked packages".format(tps.count()))
+    for tp in tps:
+        process_tracked_package(tp)
+    log.info("Done handling packages")
+    adapter = process_tracked_packages.adapter
--- marvinbot_package_tracker_plugin/models.py
+++ marvinbot_package_tracker_plugin/models.py
@@ -1,1 +1,42 @@
 import mongoengine
+from marvinbot.utils import localized_date
+
+
+class TrackedPackage(mongoengine.Document):
+    id = mongoengine.SequenceField(primary_key=True)
+    tracking_number = mongoengine.StringField(unique=True)
+
+    subscribers = mongoengine.ListField(mongoengine.LongField())
+    """Subscribers of this package."""
+
+    updates = mongoengine.StringField(null=True)
+    """Most recent updates from package."""
+
+    date_page_fetched = mongoengine.DateTimeField(null=True)
+    """Stores the last time this TrackedPackage was fetched for changes successfully."""
+
+    date_updated = mongoengine.DateTimeField(null=True)
+    """Stores the last time the package was updated."""
+
+    num_errors = mongoengine.LongField(null=True)
+
+    date_added = mongoengine.DateTimeField(default=localized_date)
+    date_modified = mongoengine.DateTimeField(default=localized_date)
+    date_deleted = mongoengine.DateTimeField(required=False, null=True)
+
+    @classmethod
+    def by_tracking_number(cls, tracking_number):
+        try:
+            return cls.objects.get(tracking_number=tracking_number)
+        except cls.DoesNotExist:
+            return None
+
+    @classmethod
+    def all(cls):
+        try:
+            return cls.objects(date_deleted=None)
+        except:
+            return None
+
+    def __str__(self):
+        return "{{ id = {id}, tracking_number = \"{tracking_number}\", updates = {updates}, subscribers = \"{subscribers}\", date_page_fetched = {date_page_fetched}, date_updated = {date_updated} }}".format(id=self.id, tracking_number=self.tracking_number, updates=self.updates, subscribers=", ".join(self.subscribers), date_page_fetched=self.date_page_fetched, date_updated=self.date_updated)
